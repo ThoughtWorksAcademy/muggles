b3468a2c46dfbe1e670abbda5a08717ecdacd9ed
ximwang <wangximingde@163.com>
2015-02-13 14:56:49 +0800

    delete useless codes

--- app/js/component/ui/checkpoint_view.js
+++ app/js/component/ui/checkpoint_view.js
@@ -10,16 +10,6 @@ define(function (require) {
         });
 
         this.renderCheckpoints = function (event, data) {
-            var checkPointTemplate = Hogan.compile(
-                '{{#checkpoints}}<li><input type="checkbox">{{checkpoint}}</li>{{/checkpoints}}'
-            );
-            var html = checkPointTemplate.render(
-                {checkpoints:[
-                    {checkpoint: 'wang'},
-                    {checkpoint: 'xi'},
-                    {checkpoint: 'ming'}
-                ]});
-            this.select('checkPointSelect').append(html);
             this.select('checkPointSelect').append(data.makeup);
             this.on('.checkpoint-item', 'click', this.handleChecked);
         };
