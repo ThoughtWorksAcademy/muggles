54fab4d8207c7500b9d6423e365f2ab946e8361c
ximwang <wangximingde@163.com>
2015-02-13 15:33:11 +0800

    fix checkbox show bug

--- app/js/component/data/checkpoint_data.js
+++ app/js/component/data/checkpoint_data.js
@@ -30,7 +30,7 @@ define(function (require) {
                 '{{^checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" disabled="true">{{name}}</li>{{/checked}}' +
                 '{{/disable}}'+
                 '{{^disable}}'+
-                '{{#checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" disabled="true" >{{name}}</li>{{/checked}}' +
+                '{{#checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" checked="true" >{{name}}</li>{{/checked}}' +
                 '{{^checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox">{{name}}</li>{{/checked}}' +
                 '{{/disable}}'+
                 '{{/checkpoints}}' +
