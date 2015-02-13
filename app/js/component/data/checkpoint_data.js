29091f1ed721980925d449153dd9c9d1f5202078
ximwang <wangximingde@163.com>
2015-02-13 14:53:46 +0800

    disable checkbox when checkpoint disable

--- app/js/component/data/checkpoint_data.js
+++ app/js/component/data/checkpoint_data.js
@@ -25,8 +25,14 @@ define(function (require) {
                 '{{#groups}}' +
                 '<p>{{groupName}}</p>' +
                 '{{#checkpoints}}' +
-                '{{#checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" checked="true" >{{name}}</li>{{/checked}}' +
+                '{{#disable}}'+
+                '{{#checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" checked="true" disabled="true">{{name}}</li>{{/checked}}' +
+                '{{^checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" disabled="true">{{name}}</li>{{/checked}}' +
+                '{{/disable}}'+
+                '{{^disable}}'+
+                '{{#checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox" disabled="true" >{{name}}</li>{{/checked}}' +
                 '{{^checked}}<li><input id="{{_id}}" class="checkpoint-item" type="checkbox">{{name}}</li>{{/checked}}' +
+                '{{/disable}}'+
                 '{{/checkpoints}}' +
                 '{{/groups}}'
             );
