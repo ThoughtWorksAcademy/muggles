7d4eeabc4fb769f093dacb2b8ead4dc285e0d64b
ximwang <wangximingde@163.com>
2015-03-05 15:52:08 +0800

    show course by type

--- app/js/component/ui/course_view.js
+++ app/js/component/ui/course_view.js
@@ -13,21 +13,21 @@ define(function (require) {
             courseName: '.course_name'
         });
         this.renderCourse = function () {
+            console.log(this.attr.course);
             var groups = this.groupCheckpoints(this.attr.course.checkpoints);
 
-            var html = template.render(groups);
-            this.select('app').append(html).fadeIn();
+            var html = template.render({groups: groups});
+            $('#app').html(html).fadeIn();
+            //this.select('app').append(html).fadeIn();
         };
 
         this.groupCheckpoints = function (checkpoints) {
             var groupCheckpoints = [];
             var groupNames = _.uniq(_.pluck(checkpoints, 'type'));
-            console.log(groupNames);
-            console.log(checkpoints);
+
             _.forEach(groupNames, function (groupName) {
                 groupCheckpoints.push({groupName: groupName, checkpoints: _.where(checkpoints, {type: groupName})})
             });
-            console.log(groupCheckpoints);
             return groupCheckpoints;
         };
 
