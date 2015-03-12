ab0fa0ea1271b17b28c977db17b480f3d3d0feac
ximwang <wangximingde@163.com>
2015-03-12 22:38:53 +0800

    fix show course of traniee

--- app/js/component/ui/station_courses.js
+++ app/js/component/ui/station_courses.js
@@ -6,16 +6,40 @@ define(function (require) {
     return defineComponent(stations);
 
     function stations() {
-        this.defaultAttrs();
+        this.defaultAttrs({
+                'traineesOfCourse': '.traineesOfCourse'
+            }
+        );
 
         this.renderStationCourses = function () {
             var courses = this.attr.stationCourses;
-            console.log(courses);
             var html = template.render({courses: courses});
             $('#app').html(html).fadeIn();
         };
 
+        this.serveCourseTrainees = function () {
+            var self = this;
+            var id = event.target.id;
+            console.log(id);
+            $.ajax('/api/courses/' + id + '/trainees/', {
+                method: 'get'
+
+            }).fail(function () {
+                console.log('获取id为' + id + '的课程的所有学生失败');
+
+            }).done(function (data) {
+                self.trigger('uiSwitchPage',
+                    {
+                        name: 'traineesOfCourse',
+                        data: data
+                    });
+            });
+        };
+
+
         this.after('initialize', function () {
+            this.on(document, 'click',
+                {'traineesOfCourse': this.serveCourseTrainees});
             this.renderStationCourses();
         });
     }
